72df5bcf0127646f7a88a034aeb768fcc93bbb28
Juana <becjuana@gmail.com>
2015-09-22 00:15:56 -0700

    Inserted d3 pie chart code

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ var app = {
             success: function(languages) {
               console.log('Success! We have retrieved languages for repo: ' + repoName);
               app.display(repoName, languages);
+              console.log('dataset:', app.pie(languages));
             },
             error: function(languages) {
               console.log('Error: unable to get languages for repo: ' + repoName);
@@ -44,27 +45,80 @@ var app = {
     _.each(languages, function(bytes) {
       totalBytes += bytes;
     });
-    $('<div class="repo" id="repoName">' + repoName + '</div>').appendTo($('.stats'));
+    $('<div class="repo" id="chart">' + repoName + '</div>').appendTo($('.stats'));
     //
     //appending text and percentages
-    _.each(languages, function(bytes, langName) {
+    _.each(languages, function(bytes, langName, languages) {
       var ratio = ((bytes / totalBytes) * 100).toFixed(1);
-      $('<div id="langName" >' + langName + ':' + ratio + '%' + '</div>')
+      $('<div class="langName">' + langName + ':' + ratio + '%' + '</div>')
         .appendTo($('.repo:last-child'));
+
+        app.createPie(languages);
     });
-    //end of each
+  },
 
-    //from tutorial
-    // var dataset = _.map(languages)
+  createPie: function(languages) {
+    var dataset = [];
+    _.each(languages, function(bytes, langName) {
+      dataset.push({label: langName, count: bytes});
+    });
 
-    // [
-    // { label: 'Abulia', count: 10 },
-    // { label: 'Betelgeuse', count: 20 },
-    // { label: 'Cantaloupe', count: 30 },
-    // { label: 'Dijkstra', count: 40 }
-    // ];
+    var width = 360;
+    var height = 360;
+    var radius = Math.min(width, height) / 2;
+    var color = d3.scale.category20b();
+    var donutWidth = 75;
+    
+    var svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
 
+    var arc = d3.svg.arc()
+    .innerRadius(radius - donutWidth) 
+    .outerRadius(radius);
 
+    var pie = d3.layout.pie()
+    .value(function(d) { return d.count; })
+    .sort(null);
+
+    var path = svg.selectAll('path')
+    .data(pie(dataset))
+    .enter()
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', function(d, i) {
+      return color(d.data.label);
+    });
+
+    var legendRectSize = 18;
+    var legendSpacing = 4;
+
+    var legend = svg.selectAll('.legend')
+    .data(color.domain())
+    .enter()
+    .append('g')
+    .attr('class', 'legend')
+    .attr('transform', function(d, i) {
+      var height = legendRectSize + legendSpacing;
+      var offset =  height * color.domain().length / 2;
+      var horz = -2 * legendRectSize;
+      var vert = i * height - offset;
+      return 'translate(' + horz + ',' + vert + ')';
+    });
+
+    legend.append('rect')
+    .attr('width', legendRectSize)
+    .attr('height', legendRectSize)
+    .style('fill', color)
+    .style('stroke', color);
+
+    legend.append('text')
+    .attr('x', legendRectSize + legendSpacing)
+    .attr('y', legendRectSize - legendSpacing)
+    .text(function(d) { return d; });
   },
 
   clearDisplay: function() {
