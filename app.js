c181e6876d65e5ed11e136bd5acccbe8ec95d856
Juana <becjuana@gmail.com>
2015-09-21 21:18:32 -0700

    No more repo name repeat per class

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
 var app = {
   init: function() {
     $(document).ready(function() {
-      // console.log('im in init');
       $('form').submit(function() {
         var username = $('input').val();
         event.preventDefault();
         $('input').val('');
         app.get(username);
+        app.clearDisplay();
       });
     });
   },
 
   get: function(username) {
-    // console.log('im in get');
     $.ajax({
       url:'https://api.github.com/users/' + username + '/repos',
       type: 'GET',
       success: function(repos) {
         console.log('Success! We have retrieved all repos for user: ' + username);
-        // console.log(repos);
+        //for each repo make another ajax request for languages
         _.each(repos, function(repo) {
           var repoName = repo.name;
-          // console.log('I am inside the each function');
-          //for each repo make a get request to get languages
           $.ajax({
             url:'https://api.github.com/repos/' + username + '/' + repoName + '/languages',
             type: 'GET',
             success: function(languages) {
               console.log('Success! We have retrieved languages for repo: ' + repoName);
-              // console.log(languages);
               app.display(repoName, languages);
 
             },
@@ -49,15 +45,16 @@ var app = {
     _.each(languages, function(bytes) {
       totalBytes += bytes;
     });
+    $('<div class="repoName">' + repoName + '</div>').appendTo($('.stats'));
     _.each(languages, function(bytes, langName) {
-      var ratio = Math.floor(bytes / totalBytes) * 100 ;
-      $('<div><div>' + repoName + '</div><div>' + langName + ':' + ratio + '%' + '</div></div>')
-        .appendTo($('#child:last-child'));
+      var ratio = ((bytes / totalBytes) * 100).toFixed(1);
+      $('<div>' + langName + ':' + ratio + '%' + '</div>')
+        .appendTo($('.repoName:last-child'));
     });
   },
 
   clearDisplay: function() {
-
+    $('#repos').children().remove();
   }
 };
 app.init();
