8f8dba74edcf58fd172dba4437c44c67b4e6fbe8
Juana <becjuana@gmail.com>
2015-09-22 00:44:27 -0700

    Lengend showing percentages

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var app = {
             success: function(languages) {
               console.log('Success! We have retrieved languages for repo: ' + repoName);
               app.display(repoName, languages);
-              console.log('dataset:', app.pie(languages));
+              app.createPie(languages, repoName);
             },
             error: function(languages) {
               console.log('Error: unable to get languages for repo: ' + repoName);
@@ -52,14 +52,14 @@ var app = {
       var ratio = ((bytes / totalBytes) * 100).toFixed(1);
       $('<div class="langName">' + langName + ':' + ratio + '%' + '</div>')
         .appendTo($('.repo:last-child'));
-
-        app.createPie(languages);
     });
   },
 
-  createPie: function(languages) {
+  createPie: function(languages, repoName) {
     var dataset = [];
+    var totalBytes = 0;
     _.each(languages, function(bytes, langName) {
+      totalBytes += bytes;
       dataset.push({label: langName, count: bytes});
     });
 
@@ -70,55 +70,55 @@ var app = {
     var donutWidth = 75;
     
     var svg = d3.select('#chart')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .append('g')
-    .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
 
     var arc = d3.svg.arc()
-    .innerRadius(radius - donutWidth) 
-    .outerRadius(radius);
+      .innerRadius(radius - donutWidth)
+      .outerRadius(radius);
 
     var pie = d3.layout.pie()
-    .value(function(d) { return d.count; })
-    .sort(null);
+      .value(function(d) { return d.count; })
+      .sort(null);
 
     var path = svg.selectAll('path')
-    .data(pie(dataset))
-    .enter()
-    .append('path')
-    .attr('d', arc)
-    .attr('fill', function(d, i) {
-      return color(d.data.label);
-    });
+      .data(pie(dataset))
+      .enter()
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', function(d, i) {
+        return color(d.data.label + ':' + (((d.data.count / totalBytes) * 100).toFixed(1)) + '%');
+      });
 
     var legendRectSize = 18;
     var legendSpacing = 4;
 
     var legend = svg.selectAll('.legend')
-    .data(color.domain())
-    .enter()
-    .append('g')
-    .attr('class', 'legend')
-    .attr('transform', function(d, i) {
-      var height = legendRectSize + legendSpacing;
-      var offset =  height * color.domain().length / 2;
-      var horz = -2 * legendRectSize;
-      var vert = i * height - offset;
-      return 'translate(' + horz + ',' + vert + ')';
-    });
+      .data(color.domain())
+      .enter()
+      .append('g')
+      .attr('class', 'legend')
+      .attr('transform', function(d, i) {
+        var height = legendRectSize + legendSpacing;
+        var offset =  height * color.domain().length / 2;
+        var horz = -2 * legendRectSize;
+        var vert = i * height - offset;
+        return 'translate(' + horz + ',' + vert + ')';
+      });
 
     legend.append('rect')
-    .attr('width', legendRectSize)
-    .attr('height', legendRectSize)
-    .style('fill', color)
-    .style('stroke', color);
+      .attr('width', legendRectSize)
+      .attr('height', legendRectSize)
+      .style('fill', color)
+      .style('stroke', color);
 
     legend.append('text')
-    .attr('x', legendRectSize + legendSpacing)
-    .attr('y', legendRectSize - legendSpacing)
-    .text(function(d) { return d; });
+      .attr('x', legendRectSize + legendSpacing)
+      .attr('y', legendRectSize - legendSpacing)
+      .text(function(d) { return d; });
   },
 
   clearDisplay: function() {
